597eb779dd9f965e1445d3c65f55151ad0d1d4ae
WooMH <woominho4756@gmail.com>
2024-07-26 17:53:22 +0900

    옵션 제작
    옵션 제작

---
+++ Assets/Scripts/OMH/Managers/ToggleManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleManager : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    public GameObject optionPanel;
+
+    private bool activeInventory = false;
+    private bool activeOption = false;
+
+    void Start()
+    {
+        inventoryPanel.SetActive(activeInventory);
+        optionPanel.SetActive(activeOption);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+    }
+
+    // aPanel이 켜진 상태에서 bPanel을 키면 aPanel 꺼지고 bPanel 켜짐
+    public void ToggleInventory()
+    {
+        if (!activeInventory)
+        {
+            if (activeOption)
+            {
+                ToggleOption(false);
+            }
+        }
+        activeInventory = !activeInventory;
+        inventoryPanel.SetActive(activeInventory);
+    }
+
+    public void ToggleOption()
+    {
+        if (!activeOption)
+        {
+            if (activeInventory)
+            {
+                ToggleInventory(false);
+            }
+        }
+        activeOption = !activeOption;
+        optionPanel.SetActive(activeOption);
+    }
+    // Panel이 true면 활성화, false면 비활성화
+    public void ToggleInventory(bool state)
+    {
+        activeInventory = state;
+        inventoryPanel.SetActive(activeInventory);
+    }
+    public void ToggleOption(bool state)
+    {
+        activeOption = state;
+        optionPanel.SetActive(activeOption);
+    }
+}
